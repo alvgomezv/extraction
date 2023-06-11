2cd737cb71c3259dd314a0bdd76fe86137594318
alvgomez <alkitegv@gmail.com>
2023-06-11 22:21:02 +0200

    Function that goes throw disk included
    Reads all disk and recovers .jpg files that were deleted
    Loading bar

--- extraction.py
+++ extraction.py
@@ -5,6 +5,12 @@ import datetime
 import time
 import ctypes
 import pytsk3
+from tabulate import tabulate
+import psutil
+from tqdm import tqdm
+
+jpg_start = b"\xff\xd8\xff\xe0\x00\x10\x4a\x46"
+jpg_end = b"\xff\xd9"
 
 #Open the usb as raw bytes
 #with open(r"\\.\\D:", "rb") as f:
@@ -41,25 +47,96 @@ def traverse_directory(directory):
             sub_directory = entry.as_directory()
             traverse_directory(sub_directory)  # Recursively traverse the sub-directory
         # Check if the entry is a file
-        elif entry.info.name.type == pytsk3.TSK_FS_NAME_TYPE_REG and entry.info.meta.type in [pytsk3.TSK_FS_META_TYPE_REG, pytsk3.TSK_FS_META_TYPE_DIR]:
+        elif entry.info.meta.type == pytsk3.TSK_FS_META_NAME_DELETED:
             # Display recovered file information
             print(f"Recovered File: {entry.info.name.name.decode()}")
+
+def print_directory_table(directory):
+    table = [["Name", "Type", "Size", "Create Date", "Modify Date"]]
+    for f in directory:
+        name = f.info.name.name
+        if f.info.meta.type == pytsk3.TSK_FS_META_TYPE_DIR:
+            f_type = "DIR"
+        else:
+            f_type = "FILE"
+        size = f.info.meta.size
+        create = f.info.meta.crtime
+        modify = f.info.meta.mtime
+        table.append([name, f_type, size, create, modify])
+    print(tabulate(table, headers="firstrow"))
 
 
 def search_deleted_files(image):
     img_info = pytsk3.Img_Info(image)
     fs_info = pytsk3.FS_Info(img_info)
     root_dir = fs_info.open_dir(path="/")
-    traverse_directory(root_dir)
+    #traverse_directory(root_dir)
+    print_directory_table(root_dir)
+
+def go_through_disk(disk):
+    # suponemos que es el disco D
+    disk_mounted = psutil.disk_partitions()[1]
+    total = psutil.disk_usage(disk_mounted.mountpoint).total
+    size = 512
+    blocks = total/size
+    count = 0
+    offset = 0
+    with tqdm(total=blocks, unit='block') as progress_bar:
+        try:
+            d = open(disk, "rb")
+        except:
+            print("Disk cannot be read, format must be: \\\\.\\\\D:")
+            exit()
+        else:
+            bytes = d.read(size)
+            progress_bar.update(1)
+            try:
+                while bytes:
+                    found = bytes.find(jpg_start)
+                    if found >= 0:
+                        drec = True
+                        print(f"Found JPG at location: {str(hex(found+(size*offset)))}")
+                        if count == 0:
+                            os.makedirs("C:\\Users\\Usuario\Desktop\\Recovered")     
+                        with open(f"C:\\Users\\Usuario\Desktop\\Recovered\\{str(count)}.jpg", "wb") as f:
+                            f.write(bytes[found:])
+                            while drec is True:
+                                bytes = d.read(size)
+                                progress_bar.update(1)
+                                found = bytes.find(jpg_end)
+                                if found >= 0:
+                                    f.write(bytes[:found+2])
+                                    d.seek((offset+1)*size)
+                                    print(f"Wrote JPG to location: {str(count)}.jpg\n")
+                                    drec = False
+                                    count += 1
+                                else:
+                                    f.write(bytes)
+                    bytes = d.read(size)
+                    progress_bar.update(1)
+                    offset += 1
+                d.close()
+            except KeyboardInterrupt:
+                progress_bar.close()
+                print("Program stopped!")
+                exit()
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Tool for recovering recently deleted files on NTFS")
     parser.add_argument("-d", "--disk", action="store", help="Path to the disk")
+    parser.add_argument("-c", "--create", action="store_true", help="Create an image from a disk file")
     parser.add_argument("-i", "--image", action="store", help="Path to the image file")
     parser.add_argument("-t", "--timelaps", action="store", help="Time range in hours, default 24h" )
     arg = parser.parse_args()
     date_format = "%d-%m-%Y"
+    if arg.image and arg.disk:
+        print("Provide either a disk or an image path")
+        exit()
+    if arg.create is True:
+        if not arg.disk:
+            print("You need to specify a disk to create an image from with the '-d' flag")
+            exit()
     try:
         if arg.timelaps is not None:
             arg.timelaps = datetime.datetime.strptime(arg.timelaps, date_format).timestamp()
@@ -72,11 +149,12 @@ def parse_arguments():
 
 if __name__ == "__main__":
     arg = parse_arguments()
-    if arg.image and arg.disk:
-        print("Provide either a disk or an image path")
     if arg.image:
         search_deleted_files(arg.image) 
     if arg.disk:
+        if arg.create is True:
+            image = create_image_from_disk(arg.disk)
+        else:
+            go_through_disk(arg.disk)
         # imput disk like this (r"\\.\\D:")
-        image = create_image_from_disk(arg.disk)
     
